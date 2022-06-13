10548eb8f3c4dc3af4300953af38f44039c35d62
Feb11F <yayanfebri43@gmail.com>
2022-06-13 19:58:01 +0700

    menambahkan file modul

---
+++ resources/views/aboutme.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+@section('content')
+<div class="container">
+    <h1 class="text-primary mt-3 mb-5">About me</h1>
+        <img src="img/peb.jpeg"   width="200px"  class="mx-auto d-block rounded mb-5">
+    <h2 class="text-primary mt-lg-5">
+        Background
+    </h2>
+    <div class="container">
+       Nama saya Febrian , saya berasal dari jombang , saya merupakan anak ke 2 dari dua bersaudara, saya bersekolah di sdn menturus , kemudian saya melanjutkan di smpn kudu , lalu saya melanjutkan di sman ploso, dan saat ini saya melanjutkan kuliah di universitas trunojoyo malang di prodi teknik informatika , alasan saya memilih prodi tersebut dikarenakan saya memilih prodi tersebut
+    </div>
+    <h2 class="text-primary mt-lg-5">
+        Suka Duka PAW
+    </h2>
+    <div class="container">
+        untuk suka mungkin saya mendapatkan ilmu tambahan diluar jam perkuliahan dan dengan metode pembelajaran yang lebih menyenangkan , untuk duka mungkin terlalu banyak tugas modul
+    </div>
+</div>
+@endsection
